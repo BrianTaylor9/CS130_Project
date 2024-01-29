57ad3d78b7b449277ed5c07ff10a83736bf5ecaf
Arjun Aggarwal <arjunaggarwal@wifi-131-179-52-243.host.ucla.edu>
2024-01-29 13:29:54 -0800

    Adding home page

---
+++ ngo-app/pages/Home.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Banner from "../components/Home/Banner";
+import "../styles/home.css";
+
+export default function Home() {
+  return (
+    <>
+      <Banner />
+      {/* <About />
+      <Collage />
+      <Events />
+      <BeVolunteer />
+      <Campaigns />
+      <Testimony />
+      <Article />
+      <Request /> */}
+    </>
+  );
+}
