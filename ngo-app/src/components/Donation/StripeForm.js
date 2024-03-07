04788653979b443454e09e50f3f00840f770646b
Arjun Aggarwal <arjunaggarwal@Arjuns-MBP-2.lan>
2024-03-06 16:15:55 -0800

    Adding comments and redirecting after payment success

--- ngo-app/src/components/Donation/StripeForm.js
+++ ngo-app/src/components/Donation/StripeForm.js
@@ -1,11 +1,12 @@
 // StripeForm.js
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 // Import Stripe elements and Material UI components
 import { useStripe, useElements, CardElement } from '@stripe/react-stripe-js';
 import { Button } from "@material-ui/core";
 import FavoriteIcon from "@mui/icons-material/Favorite";
 import Alert from '@mui/material/Alert';
 import CheckIcon from '@mui/icons-material/Check';
+import { useNavigate } from 'react-router-dom';
 
 // StripeForm component accepting props for handling donations
 const StripeForm = ({ money, onSuccessfulDonate }) => {
@@ -13,7 +14,23 @@ const StripeForm = ({ money, onSuccessfulDonate }) => {
   const elements = useElements();
   // State for managing payment success and errors
   const [paymentSuccess, setPaymentSuccess] = useState(false);
+  const [redirectCountdown, setRedirectCountdown] = useState(5); // Countdown starts from 5 seconds
   const [paymentError, setPaymentError] = useState('');
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    let timer;
+    if (paymentSuccess && redirectCountdown > 0) {
+      timer = setTimeout(() => {
+        setRedirectCountdown(redirectCountdown - 1);
+      }, 1000);
+    } else if (redirectCountdown === 0) {
+      navigate('/donate'); // Redirect to donations page when countdown reaches zero
+      window.scrollTo(0, 0);
+    }
+
+    return () => clearTimeout(timer); // Cleanup timer
+  }, [paymentSuccess, redirectCountdown, navigate]);
 
   // Function to handle donation logic
   const handleDonate = async (e) => {
@@ -72,9 +89,11 @@ const StripeForm = ({ money, onSuccessfulDonate }) => {
         </Alert>
       )}
       {paymentSuccess && (
-        <Alert icon={<CheckIcon fontSize="inherit" />} severity="success">
-          Here is a gentle confirmation that your action was successful.
-        </Alert>
+        <>
+          <Alert icon={<CheckIcon fontSize="inherit" />} severity="success">
+            Payment successful! Redirecting to donations page in {redirectCountdown} seconds...
+          </Alert>
+        </>
       )}
       <label htmlFor="card-element">Credit or Debit Card</label>
       <div className="card-element-container">
