1db652156a745aeda453028fa7b19da649297a20
Brian Taylor <btaylor9@ucla.edu>
2024-03-09 11:25:13 -0800

    moved vars to .env

--- backend/Controllers/BlogsController.js
+++ backend/Controllers/BlogsController.js
@@ -1,5 +1,6 @@
 const { MongoClient } = require("mongodb");
-const uri = "mongodb://admin:symq5uMX2gTfeWm7@ac-y855aoy-shard-00-01.ih9t6pp.mongodb.net:27017/?ssl=true&replicaSet=atlas-8r8zfl-shard-0&authSource=admin&retryWrites=true&w=majority&appName=cs130"; // connection string
+// const uri = "mongodb://admin:symq5uMX2gTfeWm7@ac-y855aoy-shard-00-01.ih9t6pp.mongodb.net:27017/?ssl=true&replicaSet=atlas-8r8zfl-shard-0&authSource=admin&retryWrites=true&w=majority&appName=cs130"; // connection string
+const uri = process.env.MONGO_URL
 const client = new MongoClient(uri);
 
 // example function for getting articles
--- backend/Middlewares/server.js
+++ backend/Middlewares/server.js
@@ -1,13 +1,15 @@
+require('dotenv').config({path: '../.env'})
 const http = require('http');
 const eventsController = require('../Controllers/EventsController');
 const blogsController = require('../Controllers/BlogsController');
+const port = parseInt(process.env.PORT);
 
 http.createServer(async (req,res)=> {
     // THIS IS FOR CORS ERRORS
     res.setHeader('Access-Control-Allow-Origin', '*'); /* @dev First, read about security */
     res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
     res.setHeader('Access-Control-Max-Age', 2592000); // 30 days
-    if(req.url == '/api/blogs'){
+    if(req.url == process.env.BLOGS_URL){
         try {
             res.writeHead(200, {'Content-Type':'application/text'});
             const dataset = await blogsController.blogs(); // here we get the string json
@@ -20,7 +22,7 @@ http.createServer(async (req,res)=> {
             res.end(); // must end response. DONT FOGET
         }
     }
-    else if(req.url == '/api/events'){
+    else if(req.url == process.env.EVENTS_URL){
         try {
             res.writeHead(200, {'Content-Type':'application/text'});
             const dataset = await eventsController.events(); // here we get the string json
@@ -33,4 +35,4 @@ http.createServer(async (req,res)=> {
             res.end(); // must end response. DONT FOGET
         }
     }
-}).listen(4000);
+}).listen(port);
